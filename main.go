f676331e65514f1d6e14d14477cc9e6e181e1912
Shawn Milochik <shawn@milochik.com>
2015-10-05 10:42:17 -0400

    print seconds elapsed in output

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+)
 
 // main is the first code run after any init() functions
 func main() {
+	start := time.Now()
 	count := 0
 	for _, repo := range getRepositories() {
 		for _, course := range getCourses(repo) {
 			count += indexCourse(course)
 		}
 	}
-	log.Printf("Indexed %d learning resources.\n", count)
+	duration := time.Now().Sub(start)
+	log.Printf("Indexed %d learning resources in %0.2f seconds.\n", count, duration.Seconds())
 }
